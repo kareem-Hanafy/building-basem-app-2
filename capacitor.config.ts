7fd8a7122e8b709ea3662ce78b25ccffca8e12e6
kareem-20 <kemoadel200180@gmail.com>
2024-10-30 10:42:52 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'building-basem-app-2',
+  webDir: 'www'
+};
+
+export default config;
